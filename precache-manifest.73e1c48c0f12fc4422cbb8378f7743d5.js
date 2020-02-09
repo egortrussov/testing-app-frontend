fe69ec510b5ac04fe3b1c185f9905ab0cd9a0275
Yegor Trussov <egortr2004@mail,ru>
2020-02-09 17:36:14 +0600

    Updates

---
+++ precache-manifest.73e1c48c0f12fc4422cbb8378f7743d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce29c0975ec699d99d363fc6fdff7534",
+    "url": "/testing-app-frontend/index.html"
+  },
+  {
+    "revision": "fdb566e3dd72742b56c3",
+    "url": "/testing-app-frontend/static/css/main.b84852c7.chunk.css"
+  },
+  {
+    "revision": "7b34fc7e6ada1d6772ab",
+    "url": "/testing-app-frontend/static/js/2.3cc5692c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/testing-app-frontend/static/js/2.3cc5692c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdb566e3dd72742b56c3",
+    "url": "/testing-app-frontend/static/js/main.e41dcd8f.chunk.js"
+  },
+  {
+    "revision": "d2e4dd1b6caa008be05b",
+    "url": "/testing-app-frontend/static/js/runtime-main.e894222f.js"
+  },
+  {
+    "revision": "de877d3a125675621dfeafae1cbc00b8",
+    "url": "/testing-app-frontend/static/media/hero.de877d3a.svg"
+  },
+  {
+    "revision": "d86e21992431dc8e82feba68135c51fb",
+    "url": "/testing-app-frontend/static/media/logo.d86e2199.svg"
+  }
+]);
